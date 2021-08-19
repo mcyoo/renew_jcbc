20369371a7d7a41e52efe43a03892aad1b607b2e
mcyoo <64882430+mcyoo@users.noreply.github.com>
2021-08-19 20:13:12 +0900

    20210819 router test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './components/home';
+import Community from './components/community';
+import Mypage from './components/mypage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className='Contents-wrapper'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/collection1' component={Community} />
+            <Route path='/collection2' component={Mypage} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+/*
+<div className="App">
+      <Router>
+        <div className='Menu-wrapper'>
+          <ul>
+            <Link to='/'><li>Home</li></Link>
+            <Link to='/community'><li>Community</li></Link>
+            <Link to='/mypage'><li>MyPage</li></Link>
+          </ul>
+        </div>
+        <div className='Contents-wrapper'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/community' component={Community} />
+            <Route path='/mypage' component={Mypage} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+*/
---
+++ src/components/home.js
@@ -0,0 +1,47 @@
+// src/components/home.js
+
+import React from 'react';
+import Slider from "react-slick";
+
+class Home extends React.Component {
+  render() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        initialSlide: 0,
+      };
+    return(
+        <div>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>0</h3>
+          </div>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+        </Slider>
+      </div>
+    )
+  }
+}
+
+export default Home
+
+// 이와 같은 방식으로 Community, Mypage 컴포넌트도 생성
