0a5a5e78e282c3dcb664fe3f710d019f49cc619b
mcyoo <64882430+mcyoo@users.noreply.github.com>
2021-09-19 11:19:24 +0900

    jcbc_art_hompage /home/

--- src/App.js
+++ src/App.js
@@ -4,14 +4,45 @@ import "./assets/App.css";
 import ensun from "./assets/img/ensun.ico";
 import season1 from "./assets/img/season1.JPG";
 import season2 from "./assets/img/season2.JPG";
+import kakao_map from "./assets/img/kakaoMap.png";
 import jcbc_logo from "./assets/img/logo.jpg";
 import kctv_jejucleanboysclub from "./assets/video/kctv_jejucleanboysclub.mp4";
 
+/*global kakao*/
 class App extends React.Component {
   state = {
     isLoading: true,
-    friend_profile: [],
+    friend_profile1: [],
+    friend_profile2: [],
+    feed_location: [],
   };
+
+  setMap = () => {
+    kakao.maps.load(() => {
+      let container = document.getElementById("Mymap");
+      let options = {
+        center: new kakao.maps.LatLng(33.371776, 126.543786),
+        level: 9,
+      };
+      const map = new window.kakao.maps.Map(container, options);
+      var imageSize = new kakao.maps.Size(25, 25);
+      var markerImage = new kakao.maps.MarkerImage(jcbc_logo, imageSize);
+
+      this.state.feed_location.map((location) => {
+        var home_Position = new kakao.maps.LatLng(location.x, location.y);
+
+        // 마커를 생성합니다
+        var home = new kakao.maps.Marker({
+          position: home_Position,
+          image: markerImage,
+        });
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        home.setMap(map);
+      });
+    });
+  };
+
   getJson = () => {
     let filename = "our_friend.json";
     let data = fetch(filename, {
@@ -27,11 +58,12 @@ class App extends React.Component {
 
   getData = async () => {
     const instagramData = await this.getJson();
-    const { friend_profile1, friend_profile2 } = instagramData;
+    const { friend_profile1, friend_profile2, feed_location } = instagramData;
 
     this.setState({
       friend_profile1,
       friend_profile2,
+      feed_location,
       isLoading: false,
     });
   };
@@ -39,11 +71,17 @@ class App extends React.Component {
   componentDidMount() {
     setTimeout(() => {
       this.getData();
-    }, 1000);
+      this.setMap();
+    }, 1500);
   }
 
   render() {
-    const { isLoading, friend_profile1, friend_profile2 } = this.state;
+    const {
+      isLoading,
+      friend_profile1,
+      friend_profile2,
+      feed_location,
+    } = this.state;
 
     const settings = {
       adaptiveHeight: true,
@@ -90,8 +128,20 @@ class App extends React.Component {
               <img
                 className="w-12 lg:w-16"
                 alt="jcbc_logo"
-                src={jcbc_logo}
+                src={kakao_map}
                 key="jcbc_logo1"
+              />
+            </div>
+          );
+        }
+        if (i === 2) {
+          return (
+            <div style={{ flex: 1 }}>
+              <img
+                className="w-12 lg:w-16"
+                alt="kakao_map"
+                src={jcbc_logo}
+                key="kakao_map"
               />
             </div>
           );
@@ -102,8 +152,44 @@ class App extends React.Component {
     return (
       <div className="">
         {isLoading ? (
-          <div className="flex justify-center relative mt-24">
-            <span className="">Loading...⏳</span>
+          <div className="w-screen overflow-x-hidden">
+            <div className="pt-0">
+              <div className="px-2">
+                <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
+                  <div className="flex items-center justify-center">
+                    <div className="bg-white p-4 rounded-md">
+                      <div className="w-2/3 h-6 bg-gray-200 rounded-full animate-pulse mb-4"></div>
+                      <div className="w-96 h-48 bg-gray-200 animate-pulse"></div>
+                      <div className="mt-8 h-32 w-full space-y-3">
+                        <div className="w-20 h-6 bg-gray-200 rounded-full animate-pulse"></div>
+                        <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
+                        <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
+                        <div className="w-1/2 h-4 bg-gray-200 rounded-full animate-pulse"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="pt-8">
+              <div className="px-2">
+                <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
+                  <div className="flex items-center justify-center">
+                    <div className="bg-white p-4 rounded-md">
+                      <div className="w-2/3 h-6 bg-gray-200 rounded-full animate-pulse mb-4"></div>
+                      <div className="w-96 h-48 bg-gray-200 animate-pulse"></div>
+                      <div className="mt-8 h-32 w-full space-y-3">
+                        <div className="w-20 h-6 bg-gray-200 rounded-full animate-pulse"></div>
+                        <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
+                        <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
+                        <div className="w-1/2 h-4 bg-gray-200 rounded-full animate-pulse"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         ) : (
           <div className="">
@@ -111,7 +197,7 @@ class App extends React.Component {
               <div className="w-screen overflow-x-hidden">
                 <div className="pt-0">
                   <div className="px-2">
-                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-lg">
+                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
                       <div className="flex">
                         <div className="w-full">
                           <div className="flex justify-between items-center p-3">
@@ -178,7 +264,7 @@ class App extends React.Component {
 
                 <div className="pt-10">
                   <div className="px-2">
-                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-lg">
+                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
                       <div className="flex">
                         <div className="w-full">
                           <div className="flex justify-between items-center p-3">
@@ -245,7 +331,7 @@ class App extends React.Component {
 
                 <div className="py-10">
                   <div className="px-2">
-                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-lg">
+                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
                       <div className="flex">
                         <div className="w-full">
                           <div className="flex justify-between items-center p-3">
@@ -326,24 +412,40 @@ class App extends React.Component {
                 </div>
               </div>
 
+              <div className="w-screen overflow-x-hidden text-center">
+                <div className="mb-6 text-lg">활동 지역</div>
+                <div
+                  onMouseDown={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                  }}
+                  onTouchStart={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                  }}
+                >
+                  <div id="Mymap" className="w-screen h-screen"></div>
+                </div>
+              </div>
+
               <div className="w-screen overflow-x-hidden">
                 <div className="text-center text-lg mb-4 text-gray-800 mt-5">
-                  Team Season1
+                  시즌1
                 </div>
-                <div className="flex flex-wrap text-sm lg:text-sm sm:text-xs md:mx-24 lg:mx-24 sm:mx-2 justify-items-auto justify-evenly">
+                <div className="flex flex-wrap text-xs md:text-sm md:mx-24 mx-2 justify-items-auto justify-evenly">
                   {friend_profile1.map((profile) => (
                     <div
-                      className="flex flex-col items-center lg:mx-10 lg:mb-12 sm:mx-2 sm:mb-6"
+                      className="flex flex-col items-center md:mx-10 md:mb-12 mx-2 mb-6"
                       key={profile.id}
                     >
                       <div className="mb-2">{profile.name}</div>
                       <div className="flex">
                         <a
                           href={profile.url}
-                          className="bg-gradient-to-r from-yellow-300 via-pink-400 to-red-500 p-1 lg:p-1 sm:p-0 rounded-full"
+                          className="bg-gradient-to-r from-yellow-300 via-pink-400 to-red-500 p-0.5 rounded-full"
                         >
                           <img
-                            className="w-20 lg:w-20 sm:w-12 rounded-full border-white border-1"
+                            className="md:w-20 w-12 rounded-full border-white border-2"
                             src={profile.img_url}
                             alt={profile.name}
                           />
@@ -353,22 +455,22 @@ class App extends React.Component {
                   ))}
                 </div>
                 <div className="text-center text-lg mb-6 text-gray-800 mt-10">
-                  Team Season2
+                  시즌2
                 </div>
-                <div className="flex flex-wrap text-sm lg:text-sm sm:text-xs mx-24 lg:mx-24 sm:mx-2 justify-items-auto justify-evenly">
+                <div className="flex flex-wrap text-xs md:text-sm mx-2 md:mx-24 justify-items-auto justify-evenly">
                   {friend_profile2.map((profile) => (
                     <div
-                      className="flex flex-col items-center lg:mx-10 lg:mb-12 sm:mx-2 sm:mb-6"
+                      className="flex flex-col items-center md:mx-10 md:mb-12 mx-2 mb-6"
                       key={profile.id}
                     >
                       <div className="mb-2">{profile.name}</div>
                       <div className="flex">
                         <a
                           href={profile.url}
-                          className="bg-gradient-to-r from-yellow-400 via-pink-400 to-red-600 p-1 lg:p-1 sm:p-0 rounded-full"
+                          className="bg-gradient-to-r from-yellow-400 via-pink-400 to-red-600 p-0.5 rounded-full"
                         >
                           <img
-                            className="w-20 lg:w-20 sm:w-12 rounded-full border-white border-1"
+                            className="w-12 md:w-20 rounded-full border-white border-2"
                             src={profile.img_url}
                             alt={profile.name}
                           />
