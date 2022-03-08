3047c9fa5d60b30b37ac977fed4877e8e283f8ca
mcyoo <64882430+mcyoo@users.noreply.github.com>
2022-03-08 09:58:46 +0900

    renew_jcbc update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends React.Component {
   state = {
     friend_profile1: [],
     friend_profile2: [],
+    friend_profile3: [],
     feed_location: [],
   };
 
@@ -23,11 +24,12 @@ class App extends React.Component {
 
   getData = async () => {
     const instagramData = await this.getJson();
-    const { friend_profile1, friend_profile2 } = instagramData;
+    const { friend_profile1, friend_profile2,friend_profile3 } = instagramData;
 
     this.setState({
       friend_profile1,
       friend_profile2,
+      friend_profile3,
     });
   };
 
@@ -36,12 +38,12 @@ class App extends React.Component {
   }
 
   render() {
-    const { friend_profile1, friend_profile2 } = this.state;
+    const { friend_profile1, friend_profile2, friend_profile3 } = this.state;
 
     return (
       <div>
         <div className="w-screen overflow-x-hidden">
-          <div className="text-center text-lg mb-4 text-gray-800">시즌1</div>
+          <div className="text-center text-lg mb-4 text-gray-800">시즌1 39명</div>
           <div className="flex flex-wrap text-xs md:text-sm md:mx-24 mx-2 justify-items-auto justify-evenly">
             {friend_profile1.map((profile) => (
               <div
@@ -65,7 +67,7 @@ class App extends React.Component {
             ))}
           </div>
           <div className="text-center text-lg mb-6 text-gray-800 mt-10">
-            시즌2
+            시즌2 62명
           </div>
           <div className="flex flex-wrap text-xs md:text-sm mx-2 md:mx-24 justify-items-auto justify-evenly">
             {friend_profile2.map((profile) => (
@@ -88,6 +90,35 @@ class App extends React.Component {
                 </div>
               </div>
             ))}
+          </div>
+
+          <div className="text-center text-lg mb-6 text-gray-800 mt-10">
+            시즌3 51명
+          </div>
+          <div className="flex flex-wrap text-xs md:text-sm mx-2 md:mx-24 justify-items-auto justify-evenly">
+            {friend_profile3.map((profile) => (
+              <div
+                className="flex flex-col items-center md:mx-10 md:mb-12 mx-2 mb-6"
+                key={profile.id}
+              >
+                <div className="mb-2">{profile.name}</div>
+                <div className="flex">
+                  <a
+                    href={profile.url}
+                    className="bg-gradient-to-r from-yellow-400 via-pink-400 to-red-600 p-0.5 rounded-full"
+                  >
+                    <img
+                      className="w-12 md:w-20 rounded-full border-white border-2"
+                      src={profile.img_url}
+                      alt={profile.name}
+                    />
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="text-center text-lg mb-6 text-gray-800 my-8">
+            탱큐 베리 감사합니다 😀
           </div>
         </div>
       </div>
